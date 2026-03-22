4cc5eef6d3951e77293bcd60a7cc40b71bb30a5e
waimea-smharris <smharris@waimea.school.nz>
2026-03-23 10:59:19 +1300

    Used segments of Prison code for the base of the project

--- src/Main.kt
+++ src/Main.kt
@@ -13,7 +13,150 @@
 
 
 
+val squares = mutableListOf<String>()
+
 fun main() {
-    println("Hello World!")
+
+    println(" Pinned")
+    createSquares()
+    createSquares()
+    showSquares()
+    slideCounter()
+
+
+    while (true) {
+        showSquares()
+        val action = getUserAction()
+        if (action == 'A') {
+            addCounter()
+        } else if (action == 'S') {
+            slideCounter()
+        } else if (action == 'Q') {
+            println("")
+            break
+        }
+
+    }
+
 }
+
+
+
+fun createSquares() {
+    for (i in 1..16) {
+        squares.add("")
+
+    }
+}
+
+fun showSquares() {
+    println("     1          2          3456")
+    print("╭──────────")
+    print("┬──────────".repeat(n = squares.size - 1))
+    println("╮")
+
+    for (cell in squares) {
+        print("│ ${cell.padEnd(length = 8)} ")
+    }
+    println("│")
+
+    print("╰──────────")
+    print("┴──────────".repeat(n = squares.size - 1))
+    print("╯")
+    for (c in squares) {
+
+    }
+    println()
+}
+
+fun getUserAction(): Char {
+    println("[A] Add Counter")
+    println("[S] Slide Counter")
+    println("[Q] Quit")
+    print("Choice: ")
+
+    val input = readlnOrNull()?.firstOrNull()?.uppercaseChar()
+    if (input == null) {
+        return 'Q'
+    }
+
+    return input
+}
+
+fun getSquareNumber(): Int {
+    print("Enter cell number (1-16): ")
+    val num = readlnOrNull()?.toIntOrNull()
+    if (num == null) {
+        println("Invalid number")
+        return -1
+
+    }
+    return num - 1
+
+}
+
+fun addCounter() {
+    print("Enter counter: ")
+    var name = readln()
+    if (name.length > 8) {
+        name = name.substring(0, 8)
+
+    }
+
+    for (i in 0..15) {
+        if (squares[i] == "") {
+            squares[i] = name
+            println("Counter added")
+            return
+
+        }
+
+    }
+
+    println("No empty squares")
+
+}
+
+
+//fun slideCounter() {
+//    println("First Square:")
+//    val c1 = getSquareNumber()
+//    println("Second Square:")
+//    val c2 = getSquareNumber()
+//    if (c1 >= 0 && c2 >= 0 && c1 < 16 && c2 < 16) {
+//        val temp = squares[c1]
+//        squares[c1] = squares[c2]
+//        squares[c2] = temp
+//        println("Counters switched")
+//
+//    }
+//
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fun getSquareNumber() {
+//
+//}
+//
+//fun slideCounter() {
+//
+//}
+//
+//fun addCounter() {
+//
+//}
 
