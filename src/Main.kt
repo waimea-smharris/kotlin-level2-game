5e4bc24738e771da3004a806b1a62b9e4c721955
waimea-smharris <smharris@waimea.school.nz>
2026-04-23 12:26:55 +1200

    Update Main.kt
    Worked on slide counter function

--- src/Main.kt
+++ src/Main.kt
@@ -14,61 +14,60 @@
 
 
 val squares = mutableListOf<String>()
-
 fun main() {
-
-    println(" Pinned \uD83D\uDCCC")
-    println("────────────────────────────────────────────────────────────────────────────────────────────────────")
-    println("Rules ")
+    println("Pinned \uD83D\uDCCC")
     println("")
-    println("### Game Setup\n" +
-
-            "- A row of 16 squares, numbered 1 to 16 from left to right\n" +
-            "- 5 counters (total) are placed randomly on the board - 4 white and 1 black\n" +
-            "- Decide who goes first\n" +
-            "\n" +
-            "### Gameplay\n" +
-            "- Players take turns - You may not skip your turn\n" +
-            "- On your turn you must do exactly one of the following:\n" +
-            "    - Slide any counter (black or white) any number of squares to the left, as long as no other counter is in the way and the destination square is empty, or...\n" +
-            "    - Remove the counter on square 1 (only if a counter is there)\n" +
-            "\n" +
-            "### Win Condition\n" +
-            "- The player who removes the black counter from square 1 wins\n" +
-            "\n" +
-            "### Variant\n" +
-            "- Counters can slide either left or right (but still can't jump other counters)\n")
-    println("────────────────────────────────────────────────────────────────────────────────────────────────────")
+    println("Rules")
+    println("____________________________________________________________")
+    println("")
+    println(
+        "### Game Setup\n" +
+                "- A row of 16 squares, numbered 1 to 16 from left to right\n" +
+                "- 5 counters are placed on the board - 4 white and 1 black\n" +
+                "- Decide who goes first\n" +
+                "\n" +
+                "### Gameplay\n" +
+                "- Players take turns\n" +
+                "- You may not skip your turn\n" +
+                "- On your turn you must do ONE of the following:\n" +
+                "- Slide any counter any number of squares to the left, as long as no other counter is in the way and the destination square is empty, or...\n" +
+                "- Remove the counter on square 1 (only if a counter is there)\n" +
+                "\n" +
+                "### Win Condition\n" +
+                "- The player who removes the black counter from square 1 wins\n" +
+                "\n" +
+                "### Variant\n" +
+                "- Counters can slide either left or right (but still can't jump other counters)\n"
+    )
+    println("____________________________________________________________")
     createSquares()
-    createSquares()
+    createCounters()
     showSquares()
-    slideCounter()
-
-
-    while (t+rue) {
+    while (true) {
         showSquares()
         val action = getUserAction()
-        if (action == 'A') {
-            addCounter()
-        } else if (action == 'S') {
+        if (action == 1) {
+            removeCounter()
+        } else if (action == 2) {
             slideCounter()
-        } else if (action == 'Q') {
-            println("---")
-            break
         }
-
     }
-
 }
-
-
-
 fun createSquares() {
-    for (i in 1..8)
-        squares.add("---")
-
+    squares.clear()
+    for (i in 1..16) {
+        squares.add(" ")
     }
-
+}
+fun createCounters() {
+    val randomPositions = (0..15).shuffled().take(5)
+    // First one is black
+    squares[randomPositions[0]] = "[B]"
+    // Other 4 are white
+    for (i in 1 until randomPositions.size) {
+        squares[randomPositions[i]] = "[W]"
+    }
+}
 fun showSquares() {
     println("   1     2     3     4     5     6     7     8     9     10    11    12    13    14    15    16")
     print("╭─────")
@@ -89,73 +88,129 @@ fun showSquares() {
     println()
 }
 
-fun getUserAction(): Char {
-    println("[A] Add Counter")
-    println("[S] Slide Counter")
-    println("[Q] Quit")
-    print("Choice: ")
-
-    val input = readlnOrNull()?.firstOrNull()?.uppercaseChar()
-    if (input == null) {
-        return 'Q'
-    }
-
-    return input
+fun getUserAction(): Int {
+    println("Choose an action:")
+    println("1. Remove counter from square 1")
+    println("2. Slide a counter")
+    print("Enter choice: ")
+    return readLine()?.toIntOrNull() ?: 0
 }
-
-fun getSquareNumber(): Int {
-    print("Enter cell number (1-16): ")
-    val num = readlnOrNull()?.toIntOrNull()
-    if (num == null) {
-        println("Invalid number")
-        return -1
-
+fun removeCounter() {
+    if (squares[0] == "") {
+        println("There is no counter on square 1")
+        return
     }
-    return num - 1
-
-}
-
-fun addCounter() {
-    print("Enter counter: ")
-    var name = readln()
-    if (name.length > 8) {
-        name = name.substring(0, 8)
-
+    if (squares[0] == "[B]") {
+        println("There is no counter on square 1")
+        println("You win!")
+        kotlin.system.exitProcess(0)
+        return
+    }else {
+        println("White counter removed from square 1")
+        squares[0] = " "
     }
 
-    for (i in 0..15) {
-        if (squares[i] == "---") {
-            squares[i] = name
-            println("Counter added")
-            return
 
-        }
-
-    }
-
-    println("No empty squares")
 
 }
-
 
 fun slideCounter() {
-    println("First Square:")
-    val c1 = getSquareNumber()
-    println("Second Square:")
-    val c2 = getSquareNumber()
-    if (c1 >= 0 && c2 >= 0 && c1 < 8 && c2 < 8) {
-        val temp = squares[c1]
-        squares[c1] = squares[c2]
-        squares[c2] = temp
-        println("Counters switched")
-
+    print("Enter counter position to move (1-16): ")
+    val from = (readLine() ?.toIntOrNull() ?: return) - 1
+    print("Enter new position (must move left): ")
+    val to = (readLine()?.toIntOrNull() ?: return) - 1
+    if (from !in 0..15  || to !in 0..15) {
+        println("Invalid position")
+        return
     }
-
+    if (squares[from] == "") {
+        println("No counter there")
+        return
+    }
+    if (to > from) {
+        println("Must move left only")
+        return
+    }
+    if (squares[to] == " ") {
+        println("Cannot jump over another counter")
+        return
+    }
+    for (i in (to + 1) until from) {
+        if (squares[i] == "  ") {
+            println("Cannot jump over another counter")
+            return
+        }
 }
 
 
-
-
+}
+//fun getUserAction(): Char {
+//    println("[A] Add Counter")
+//    println("[S] Slide Counter")
+//    println("[Q] Quit")
+//    print("Choice: ")
+//
+//    val input = readlnOrNull()?.firstOrNull()?.uppercaseChar()
+//    if (input == null) {
+//        return 'Q'
+//    }
+//
+//    return input
+//}
+//
+//fun getSquareNumber(): Int {
+//    print("Enter cell number (1-16): ")
+//    val num = readlnOrNull()?.toIntOrNull()
+//    if (num == null) {
+//        println("Invalid number")
+//        return -1
+//
+//    }
+//    return num - 1
+//
+//}
+//
+//fun addCounter() {
+//    print("Enter counter: ")
+//    var name = readln()
+//    if (name.length > 8) {
+//        name = name.substring(0, 8)
+//
+//    }
+//
+//    for (i in 0..15) {
+//        if (squares[i] == "---") {
+//            squares[i] = name
+//            println("Counter added")
+//            return
+//
+//        }
+//
+//    }
+//
+//    println("No empty squares")
+//
+//}
+//
+//
+//fun slideCounter() {
+//    println("First Square:")
+//    val c1 = getSquareNumber()
+//    println("Second Square:")
+//    val c2 = getSquareNumber()
+//    if (c1 >= 0 && c2 >= 0 && c1 < 8 && c2 < 8) {
+//        val temp = squares[c1]
+//        squares[c1] = squares[c2]
+//        squares[c2] = temp
+//        println("Counters switched")
+//
+//    }
+//
+//}
+//
+//
+//
+//
 
 
 
